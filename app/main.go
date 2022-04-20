514be77fc50235741d03960ef861f4883d093961
Marcin Jasion <marcinjasion@gmail.com>
2022-04-20 23:42:18 +0200

    rewritten app, removed istio gateway, added curl pod, swiched to statefulsets

--- app/main.go
+++ app/main.go
@@ -1,63 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "log"
-    "net/http"
-    "time"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-    http.HandleFunc("/wrong", HandlePostWrong);
-    http.HandleFunc("/correct", HandlePostCorrect);
-    http.HandleFunc("/health", HandleHealth);
+	http.HandleFunc("/wrong", HandlePostWrong)
+	http.HandleFunc("/correct", HandlePostCorrect)
+	http.HandleFunc("/health", HandleHealth)
 	log.Println("starting")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 func HandleHealth(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 }
 func HandlePostWrong(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Connection", "Keep-Alive")
-    w.Header().Set("Transfer-Encoding", "Chunked")
-    w.Header().Set("X-Content-Type-Options", "nosniff")
-
-    ticker := time.NewTicker(time.Second)
-    go func() {
-        for t := range ticker.C {
-            io.WriteString(w, "Chunk\n")
-            fmt.Println("Tick at", t)
-        }
-    }()
-    time.Sleep(time.Millisecond * 5100)
-    ticker.Stop()
-    fmt.Println("Finished: should return Content-Length: 0 here")
-    w.Header().Set("Content-Length", "0")
+	sendChunkedResponse(w, r, "Chunked")
 }
 
 func HandlePostCorrect(w http.ResponseWriter, r *http.Request) {
-    // #1 add flusher
-    flusher, ok := w.(http.Flusher)
-    if !ok {
-        panic("expected http.ResponseWriter to be an http.Flusher")
-    }
-    w.Header().Set("Connection", "Keep-Alive")
-    // Don't need these this bc manually flushing sets this header
-    // w.Header().Set("Transfer-Encoding", "chunked")
-    
-    // make sure this header is set
-    w.Header().Set("X-Content-Type-Options", "nosniff")
+	sendChunkedResponse(w, r, "chunked")
+}
 
-    ticker := time.NewTicker(time.Second)
-    go func() {
-        for t := range ticker.C {
-            io.WriteString(w, "Chunk\n")
-            fmt.Println("Tick at", t)
-            flusher.Flush()
-        }
+func sendChunkedResponse(w http.ResponseWriter, r *http.Request, te string) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		panic("expected http.ResponseWriter to be an http.Flusher")
+	}
+	w.Header().Set("Connection", "Keep-Alive")
+	w.Header().Set("Transfer-Encoding", te)
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for t := range ticker.C {
+			io.WriteString(w, "Chunk\n")
+			fmt.Println("Tick at", t)
+			flusher.Flush()
+		}
 		fmt.Println("Closed")
-    }()
-    time.Sleep(time.Millisecond * 5100)
+	}()
+	time.Sleep(time.Millisecond * 5100)
 	ticker.Stop()
 }
